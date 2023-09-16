f6bb58523750cfd4bd441ab99a1b8b7a818b9e39
Sakha Gangadhar <vsakha@uncc.edu>
2023-09-16 18:49:50 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,47 +3,13 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
-const budget = {
-   myBudget : [
-    {
-        "title": "Eat out",
-        "budget": 30
-    },
-    {
-        "title": "Rent",
-        "budget": 350
-    },
-    {
-        "title": "Groceries",
-        "budget": 90
-    },
-    {
-       "title": "Kitchenware",
-       "budget" : 110
-    },
-    {
-        "title": "Water bill",
-        "budget": 35
-    },
-    {
-       "title": "Wifi bill",
-        "budget": 40
-    },
-    {
-        "title": "Clothes",
-        "budget": 150
-    }
-
-]
-};  
-
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.use('/budget', (req, res) => {
-    res.json(budget);
+    res.sendFile("C:\\xampp\\Dev\\Week 03\\personal-budget1\\server.json");
 })
 
 
